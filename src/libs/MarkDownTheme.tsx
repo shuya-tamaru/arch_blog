78735e6a6ef434c1fd6a32ddc95e28591ad288a7
ShuyaTamaru <tamarushuuya@mba.local>
2023-01-28 15:12:07 +0900

    markdown theme

--- src/libs/MarkDownTheme.tsx
+++ src/libs/MarkDownTheme.tsx
@@ -1,16 +1,57 @@
 import { Text } from "@chakra-ui/react";
 import CodeBlack from "../components/CodeBlack";
 
+const hStyle = {
+  borderBottom: "2px solid #999",
+  mb: "2px",
+  fontWeight: 800,
+};
+
 export const markdownTheme = {
   h1: (props: any) => {
     const { children } = props;
     return (
-      <Text
-        fontWeight={600}
-        borderBottom="2px solid #333"
-        mb={2}
-        fontSize={"40px"}
-      >
+      <Text fontSize="6xl" sx={hStyle}>
+        {children}
+      </Text>
+    );
+  },
+  h2: (props: any) => {
+    const { children } = props;
+    return (
+      <Text fontSize="5xl" sx={hStyle}>
+        {children}
+      </Text>
+    );
+  },
+  h3: (props: any) => {
+    const { children } = props;
+    return (
+      <Text fontSize="4xl" sx={hStyle}>
+        {children}
+      </Text>
+    );
+  },
+  h4: (props: any) => {
+    const { children } = props;
+    return (
+      <Text fontSize="3xl" sx={hStyle}>
+        {children}
+      </Text>
+    );
+  },
+  h5: (props: any) => {
+    const { children } = props;
+    return (
+      <Text fontSize="2xl" sx={hStyle}>
+        {children}
+      </Text>
+    );
+  },
+  h6: (props: any) => {
+    const { children } = props;
+    return (
+      <Text fontSize="xl" sx={hStyle}>
         {children}
       </Text>
     );
